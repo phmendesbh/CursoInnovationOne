3be60b302e6bce27591ba37fdf31b09216f3ada5
Paulo Mendes <motoredi@yahoo.com.br>
2020-08-19 13:07:50 -0300

    JavaScript - aula3

--- aula_javascript/js/main.js
+++ aula_javascript/js/main.js
@@ -46,6 +46,27 @@ alert("Bem vindo " + nome + " tem " + idade + " anos");
 // console.log(fruta);
 // console.log(fruta.nome);
 
-var frutas = [{nome:"maca", cor:"vermelho"}, {nome:"uva", cor:"roxo"}]
-console.log(frutas);
-console.log(frutas[1].nome);
+// var frutas = [{nome:"maca", cor:"vermelho"}, {nome:"uva", cor:"roxo"}]
+// console.log(frutas);
+// console.log(frutas[1].nome);
+
+// var idade = prompt("Qual sua idade:")
+// if(idade >= 18) {
+//     alert("maior de idade");
+// } else {
+//     alert("menor de idade");
+// };
+
+// var count = 0;
+// while (count <5){
+//     console.log(count);
+//     count++;
+// }
+
+// for(var x=5; x < 10; x++){
+//     console.log(x);
+// }
+
+var d = new Date();
+alert(d);
+alert(d.getMonth());
