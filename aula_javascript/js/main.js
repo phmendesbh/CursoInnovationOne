aafc7842100efa6cc5b408db6260ad61f482b892
Paulo Mendes <motoredi@yahoo.com.br>
2020-08-19 11:24:25 -0300

    JavaScript - aula1

--- aula_javascript/js/main.js
+++ aula_javascript/js/main.js
@@ -1,1 +1,28 @@
-alert("meu primeiro js")
+/*
+var nome = "Paulo";
+var idade = 29;
+alert("Bem vindo " + nome + " tem " + idade + " anos");
+*/
+
+// var idade = 29;
+// var idade2 = 9;
+// alert(idade + idade2);
+
+//  var idade = 29;
+//  var idade2 = "9";
+// alert(idade + idade2);
+
+//No inspecionar do navegador
+// console.log(idade);
+// console.log(idade2);
+
+// var frase = "Japão é o melhor time do mundo";
+// console.log(frase.replace("Japão","Brasil"))
+// console.log(frase.toUpperCase())
+
+var n1 = 5;
+var n2 = 3;
+console.log(n1 + n2);
+console.log(n1 - n2);
+console.log(n1 * n2);
+console.log(n1 / n2);
